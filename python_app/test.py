8e827762c52fe7a9ad99c73087b34337c2d57a02
Zalvarezz <34946000+Zalvarezz@users.noreply.github.com>
2022-09-17 13:22:55 +0200

    Update test.py

--- python_app/test.py
+++ python_app/test.py
@@ -2,7 +2,7 @@ import serial
 import time
 if __name__ == '__main__':
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.flush()
+  ser.flush()
 while True:
 ser.write(b"Hello from Raspberry Pi!\n")
 line = ser.readline().decode('utf-8').rstrip()
