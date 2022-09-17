cfb991a9c682a3c72d622f8382f128786356f726
Zalvarezz <34946000+Zalvarezz@users.noreply.github.com>
2022-09-17 13:23:54 +0200

    Update test.py

--- python_app/test.py
+++ python_app/test.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
   ser.flush()
 while True:
-ser.write(b"Hello from Raspberry Pi!\n")
-line = ser.readline().decode('utf-8').rstrip()
-print(line)
-time.sleep(1)
+  ser.write(b"Hello from Raspberry Pi!\n")
+  line = ser.readline().decode('utf-8').rstrip()
+  print(line)
+  time.sleep(1)
